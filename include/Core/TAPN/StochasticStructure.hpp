37e008d924ab0bf8fdd8c0debc20927348bf38a4
Adam Galla <me@adamgalla.com>
2024-12-18 22:16:24 +0100

    Update StuchasticStructure

--- include/Core/TAPN/StochasticStructure.hpp
+++ include/Core/TAPN/StochasticStructure.hpp
@@ -7,111 +7,96 @@
 
 namespace VerifyTAPN::SMC {
 
-    enum FiringMode {
-        Oldest,
-        Youngest,
-        Random
-    };
+enum FiringMode { Oldest, Youngest, Random };
 
-    std::string firingModeName(FiringMode type);
+std::string firingModeName(FiringMode type);
 
-    enum DistributionType {
-        Constant,
-        Uniform,
-        Exponential,
-        Normal,
-        Gamma,
-        Erlang,
-        DiscreteUniform,
-        Geometric
-    };
+enum DistributionType { Constant, Uniform, Exponential, Normal, Gamma, Erlang, DiscreteUniform, Geometric };
 
-    std::string distributionName(DistributionType type);
+std::string distributionName(DistributionType type);
 
-    struct SMCUniformParameters {
-        double a;
-        double b;
-    };
-    struct SMCExponentialParameters {
-        double rate;
-    };
-    struct SMCNormalParameters {
-        double mean;
-        double stddev;
-    };
-    struct SMCConstantParameters {
-        double value;
-    };
-    struct SMCGammaParameters {
-        double shape;
-        double scale;
-    };
-    struct SMCDiscreteUniformParameters {
-        int a;
-        int b;
-    };
-    struct SMCGeometricParameters {
-        double p;
-    };
+struct SMCUniformParameters {
+  double a;
+  double b;
+};
+struct SMCExponentialParameters {
+  double rate;
+};
+struct SMCNormalParameters {
+  double mean;
+  double stddev;
+};
+struct SMCConstantParameters {
+  double value;
+};
+struct SMCGammaParameters {
+  double shape;
+  double scale;
+};
+struct SMCDiscreteUniformParameters {
+  int a;
+  int b;
+};
+struct SMCGeometricParameters {
+  double p;
+};
 
-    union DistributionParameters {
-        SMCUniformParameters uniform;
-        SMCExponentialParameters exp;
-        SMCNormalParameters normal;
-        SMCConstantParameters constant;
-        SMCGammaParameters gamma;
-        SMCDiscreteUniformParameters discreteUniform;
-        SMCGeometricParameters geometric;
-    };
+union DistributionParameters {
+  SMCUniformParameters uniform;
+  SMCExponentialParameters exp;
+  SMCNormalParameters normal;
+  SMCConstantParameters constant;
+  SMCGammaParameters gamma;
+  SMCDiscreteUniformParameters discreteUniform;
+  SMCGeometricParameters geometric;
+};
 
-    struct Distribution {
-        DistributionType type;
-        DistributionParameters parameters;
+struct Distribution {
+  DistributionType type;
+  DistributionParameters parameters;
 
-        template<typename T>
-        double sample(T& engine, const unsigned int precision = 0) const {
-            double date = 0;
-            switch(type) {
-                case Constant:
-                    date = parameters.constant.value;
-                    break;
-                case Uniform:
-                    date = std::uniform_real_distribution(parameters.uniform.a, parameters.uniform.b)(engine);
-                    break;
-                case Exponential:
-                    date = std::exponential_distribution(parameters.exp.rate)(engine);
-                    break;
-                case Normal:
-                    date = std::normal_distribution(parameters.normal.mean, parameters.normal.stddev)(engine);
-                    break;
-                case Gamma:
-                case Erlang:
-                    date = std::gamma_distribution(parameters.gamma.shape, parameters.gamma.scale)(engine);
-                    break;
-                case DiscreteUniform:
-                    date = std::uniform_int_distribution(parameters.discreteUniform.a, parameters.discreteUniform.b)(engine);
-                    break;
-                case Geometric:
-                    date = std::geometric_distribution(parameters.geometric.p)(engine);
-                    break;
-            }
-            if(precision > 0) {
-                double factor = pow(10.0, precision);
-                date = round(date * factor) / factor;
-            }
-            return std::max(date, 0.0);
-        }
+  template <typename T> double sample(T &engine, const unsigned int precision = 0) const {
+    double date = 0;
+    switch (type) {
+    case Constant:
+      date = parameters.constant.value;
+      break;
+    case Uniform:
+      date = std::uniform_real_distribution<double>(parameters.uniform.a, parameters.uniform.b)(engine);
+      break;
+    case Exponential:
+      date = std::exponential_distribution<double>(parameters.exp.rate)(engine);
+      break;
+    case Normal:
+      date = std::normal_distribution<double>(parameters.normal.mean, parameters.normal.stddev)(engine);
+      break;
+    case Gamma:
+    case Erlang:
+      date = std::gamma_distribution<double>(parameters.gamma.shape, parameters.gamma.scale)(engine);
+      break;
+    case DiscreteUniform:
+      date = std::uniform_int_distribution<double>(parameters.discreteUniform.a, parameters.discreteUniform.b)(engine);
+      break;
+    case Geometric:
+      date = std::geometric_distribution<double>(parameters.geometric.p)(engine);
+      break;
+    }
+    if (precision > 0) {
+      double factor = pow(10.0, precision);
+      date = round(date * factor) / factor;
+    }
+    return std::max(date, 0.0);
+  }
 
-        static Distribution urgent();
+  static Distribution urgent();
 
-        static Distribution defaultDistribution();
+  static Distribution defaultDistribution();
 
-        static Distribution fromParams(int distrib_id, double param1, double param2);
+  static Distribution fromParams(int distrib_id, double param1, double param2);
 
-        std::string toXML() const;
+  std::string toXML() const;
+};
 
-    };
-
-}
+} // namespace VerifyTAPN::SMC
 
 #endif
